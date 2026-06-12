cd0b7ce29475cae98c79a259bc7401bff4b932d6
JoseRamirez <jr199705@gmial.com>
2026-06-11 21:51:01 -0600

    ¨save extract-data function before merging

---
+++ main.py
@@ -0,0 +1,16 @@
+def extract_data(file_path):
+   
+    try:
+        df = pd.read_csv(file_path)
+        print("[INFO] Datos extraídos correctamente.")
+        return df
+    except Exception as e:
+        print(f"[ERROR] No se pudo leer el archivo: {e}")
+        return None
+    
+datos_cargados = extract_data("datos_clientes.csv")
+
+# Si el archivo tiene datos, mostramos los primeros 5 en la pantalla
+if datos_cargados is not None:
+    print("\n--- Mostrando las primeras filas del archivo ---")
+    print(datos_cargados.head())
